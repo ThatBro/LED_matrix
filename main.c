0db27c4d1609438db5db1a0c1fe0843b7e6d941c
ThatBro <jellesinnige@hotmail.com>
2017-05-22 13:41:08 +0200

    solved major bug

--- main.c
+++ main.c
@@ -1179,9 +1179,13 @@ int tetris_remove_line(ws2811_led_t* gamefield) {
 int tetris_eval_gamestate(ws2811_led_t* gamefield) {
 	int i;
 	int score = 0;
+	int scoremult = height*2;
 	for (i = 0; i < width*height; i++) {
+		if (i%width == 0) {
+			scoremult /= 2;
+		}
 		if (gamefield[i] == 0) {
-			score += (i/width);
+			score += scoremult;
 		}
 	}
 	return score;	
